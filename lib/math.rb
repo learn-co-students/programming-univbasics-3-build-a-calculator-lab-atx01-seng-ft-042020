78e891e03cc43d0d11f60539b191c75dc92d85df
Philip Roush <roush.philip@gmail.com>
2020-04-03 23:49:58 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
-def addition(num1, num2)
+def addition(num1 = 68, num2 = 1)
+    phil = num1 + num2
+    return phil
+end
+def subtraction(num1 = 100, num2 = 31)
+    roush = num1 - num2
+    return roush
+end
+
+def division(num1 = 100, num2 = 10)
+    ass = num1 / num2
+    return ass
+end
+
+def multiplication(num1= 6, num2= 9)
+  nixtysine = num1 * num2
+  return nixtysine
 
 end
 
-def subtraction(num1, num2)
+def modulo(num1= 6, num2 = 9)
+  sixtynine = num1 % num2
+  return sixtynine
 
 end
 
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
+def square_root(num = 69)
+  myass = Math.sqrt(num)
+  return myass
 
 end
